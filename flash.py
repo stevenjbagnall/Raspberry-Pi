7d59279c40e8ecea5be8c4dee743e440b652cf17
Steve Bagnall <stevenjbagnall@yahoo.co.uk>
2015-05-03 16:56:59 +0100

    Create flash.py

---
+++ flash.py
@@ -0,0 +1,25 @@
+#import the library to control the GPIO pins
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BOARD)
+
+#import the time library
+import time
+
+led_pin = 8
+
+#set the pin to be an output
+GPIO.setup(led_pin, GPIO.OUT)
+
+#in a loop:
+while True:
+    #turn on the led
+    print("True")
+    GPIO.output(led_pin, True)
+    #wait for 0.5 seconds
+    time.sleep(0.5)
+
+    #turn off the led
+    print("False")
+    GPIO.output(led_pin, False)
+    #sleep again
+    time.sleep(0.5)
